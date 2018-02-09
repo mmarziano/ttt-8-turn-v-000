db17ddf1b5160dabdfe77125d6e411f751d5d202
Monica Marziano <mmarziano1@gmail.com>
2018-02-09 10:16:15 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,12 @@ end
 
 
 def move(board, index, char = "X")
-
+  board[index] = ${char}
+  puts " " + board[0] + " " + "|" + " " + board[1] + " " + "|" + " " + board[2] + " "
+  puts "-----------"
+  puts " " + board[3] + " " + "|" + " " + board[4] + " " + "|" + " " + board[5] + " "
+  puts "-----------"
+  puts " " + board[6] + " " + "|" + " " + board[7] + " " + "|" + " " + board[8] + " "
 end
 
 def turn(board)
